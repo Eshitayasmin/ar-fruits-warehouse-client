08b94205d35b6a540153607434f625b32465a76b
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-05-07 10:05:05 +0600

    blogs add

--- src/Pages/Update/Update.js
+++ src/Pages/Update/Update.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useFruitDetail from '../../hooks/useFruitDetail';
 import taka from '../../images/taka.png';
 import './Update.css';
 
 const Update = () => {
-    const {id} = useParams();
-    const [updateItem, setUpdateItem] = useState({});
+    const { id } = useParams();
+    const [fruitDetail] = useFruitDetail(id);
+    const { name, price, quantity, img, supplierName, description } = fruitDetail;
 
-    useEffect(() =>{
-        fetch(`http://localhost:5000/inventory/${id}`)
-        .then(res => res.json())
-        .then(data => setUpdateItem(data));
-    }, [id]);
+    const [number, setNumber] = useState(true);
+
+
+    const handleDelivered = () => {
+        let newQuantity = quantity - 1;
+    }
+
 
 
     return (
-        <div>
-             <div className='update-item w-50 mx-auto'>
-            <img className='update-item-image' src={updateItem.img} alt="" />
-            <h3>{updateItem.name}</h3>
-            <p>{updateItem.description}</p>
-            <h6>Price: <img className='taka-icon' src={taka} alt="" /> <span className='mt-3 px-0'>{updateItem.price}</span></h6>
-            <p>Quantity: {updateItem.quantity}</p>
-            <p><small>Supplier: {updateItem.supplierName}</small></p>
-            
-        </div>
-        </div>
+         
+                <div className='update-item mx-auto'>
+                <div>
+                    <img className='update-item-image' src={img} alt="" />
+                </div>
+                <div>
+                    <h3>{name}</h3>
+                    <p>{description}</p>
+                    <p><small>Supplier: {supplierName}</small></p>
+                    <h6>Price: <img className='taka-icon' src={taka} alt="" /> <span className='mt-3 px-0'>{price}</span></h6>
+                    <h6>Quantity: {quantity}</h6>
+                    <button onClick={handleDelivered} className='mt-3 my-2 delivery-btn'>Delivered</button>
+                   <div>
+                   <input className='me-3 mb-2' type="number" placeholder='Restock quantity'/>
+                    <button className='restock-btn'>Restock</button>
+                   </div>
+                </div>
+            </div>
+          
     );
 };
 
